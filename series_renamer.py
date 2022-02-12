4b54114e30fe99b57e911b88e54d8f1d5401cba1
unknown <mccaffrey.scott@gmail.com>
2022-02-12 12:32:12 -0700

    functionalize

--- series_renamer.py
+++ series_renamer.py
@@ -3,26 +3,59 @@
 import sys
 import os
 from os import path
-
-print("Welcome to the TV Series Batch Renamer")
-
-
-series_name = input("Series Name:" )
-season_num = input("Season Number: ")
-series_path = input("Series Path: ")
-series_ext = input("Series File Extension: ")
-series_url = input("Wikipedia URL: ")
-
-print()
+#import pandas as pd
+#import wikipedia as wp
 
 
-if not path.exists(series_path):
-    sys.exit("Series Path INVALID")
+def get_args():
+    series_name = input("Series Name:" )
+    season_num = input("Season Number: ")
+    series_path = input("Series Path: ")
+    series_ext = input("Series File Extension: ")
+    series_url = input("Wikipedia URL: ")
 
-files = os.listdir(series_path)
-for file in files:
-    if os.path.isfile(os.path.join(series_path, file)):
-        print(file)
+    print()
 
-print(series)
-print(url)
+    return series_name, season_num, series_path, series_ext, series_url
+
+
+def test_args():
+    series_name = "Squid Game"
+    season_num = 1
+    series_path = "./Squid.Game.S01.DUBBED.WEBRip.x265-ION265"
+    series_ext = "mp4"
+    series_url = "https://en.wikipedia.org/wiki/Squid_Game"
+
+    return series_name, season_num, series_path, series_ext, series_url
+
+
+def main():
+    print("Welcome to the TV Series Batch Renamer")
+
+    series_name, season_num, series_path, series_ext, series_url = get_args()
+    series_name, season_num, series_path, series_ext, series_url = test_args()
+    
+    if not path.exists(series_path):
+        sys.exit("Series Path INVALID")
+
+    files = os.listdir(series_path)
+    for file in files:
+        if os.path.isfile(os.path.join(series_path, file)):
+            print(file)
+
+    print(series_name)
+    print(series_url)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
