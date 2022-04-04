296f56871d5f51ce1cbac8cc28d69b0cbfa1dfd0
unknown <mccaffrey.scott@gmail.com>
2022-04-03 21:09:08 -0600

    New libs, working

--- series_renamer.py
+++ series_renamer.py
@@ -3,8 +3,10 @@
 import sys
 import os
 from os import path
-#import pandas as pd
-#import wikipedia as wp
+import pandas as pd
+import wikipedia as wp
+import re
+
 
 
 def get_args():
@@ -24,38 +26,58 @@ def test_args():
     season_num = 1
     series_path = "./Squid.Game.S01.DUBBED.WEBRip.x265-ION265"
     series_ext = "mp4"
-    series_url = "https://en.wikipedia.org/wiki/Squid_Game"
+    series_url = "Squid_Game#Episodes"
 
     return series_name, season_num, series_path, series_ext, series_url
+
+
+def scrape_wikipedia(series_url):
+    html = wp.page(series_url).html().encode("UTF-8")
+    try: 
+        df = pd.read_html(html)[1]
+    except IndexError:
+        df = pd.read_html(html)[0]
+    #print(df.to_string())
+
+    episode_names = []
+    bitflip = True
+    for index, row in df.iterrows():
+        if (bitflip):
+            name = re.findall('"([^"]*)"', row[1])[0]
+            episode_names.append(name)
+            bitflip = False
+        else:
+            bitflip = True
+    
+    return episode_names
+
 
 
 def main():
     print("Welcome to the TV Series Batch Renamer")
 
-    series_name, season_num, series_path, series_ext, series_url = get_args()
+    #setup
+    #series_name, season_num, series_path, series_ext, series_url = get_args()
     series_name, season_num, series_path, series_ext, series_url = test_args()
-    
+
+    #get table from wikipedia
+    episode_names = scrape_wikipedia(series_url)
+    for name in episode_names:
+        print(name)
+
+    #find local series files
     if not path.exists(series_path):
         sys.exit("Series Path INVALID")
 
     files = os.listdir(series_path)
-    for file in files:
-        if os.path.isfile(os.path.join(series_path, file)):
-            print(file)
+    #for file in files:
+     #   if os.path.isfile(os.path.join(series_path, file)):
+            #print(file)
 
-    print(series_name)
-    print(series_url)
+    #print(series_name)
+    #print(series_url)
 
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
